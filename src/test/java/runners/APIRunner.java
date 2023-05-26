dfb7f873bab866231b6681a7ccee2544d472929f
Erman Coskun <115878270+ermancoskun@users.noreply.github.com>
2023-05-27 00:56:55 +0300

    erman 2705 v5

--- src/test/java/runners/APIRunner.java
+++ src/test/java/runners/APIRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = {"stepDefinitions", "hooks"},
-        tags = "@US15",
+        tags = "@US25",
         dryRun = false
 
 
