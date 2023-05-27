518492aa114ea5c7e0618d5e1ac65ac5ea51d9e2
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-05-28 00:52:59 +0300

    28.05.23 v2 ayazicioglu

--- src/test/java/stepDefinitions/APIStepDefinition.java
+++ src/test/java/stepDefinitions/APIStepDefinition.java
@@ -36,34 +36,6 @@ public class APIStepDefinition {
     String message;
 
 
-    @Given("Api kullanicisi {string} path parametreleri set eder")
-    public void api_kullanicisi_path_parametreleri_set_eder(String rawPaths) {
-
-        String[] paths = rawPaths.split("/"); // ["api","register"]
-
-        StringBuilder tempPath = new StringBuilder("/{");
-
-        for (int i = 0; i < paths.length; i++) {
-
-
-            String key = "pp" + (i + 1); // pp1 pp2 pp3
-
-
-            String value = paths[i].trim();
-
-            HooksAPI.spec.pathParam(key, value);
-
-            tempPath.append(key + "}/{");
-        }
-
-        tempPath.deleteCharAt(tempPath.lastIndexOf("{"));
-        tempPath.deleteCharAt(tempPath.lastIndexOf("/"));
-
-
-        fullPath = tempPath.toString(); // /{pp0}/{pp1}/{pp2}
-
-        System.out.println("fullPath = " + fullPath);
-    }
 
     @Given("Api user sets {string} path parameters.")
     public void apiUserSetsPathParameters(String rawPaths) {
@@ -375,18 +347,6 @@ public class APIStepDefinition {
         Assert.assertTrue(message.contains("403"));
 
 
-    }
-
-
-    @Given("Get query is generated with valid information")
-    public void getQueryIsGeneratedWithValidInformation() {
-
-        response = given()
-                .spec(HooksAPI.spec)
-                .header("Authorization", "Bearer " + HooksAPI.token)
-                .contentType(ContentType.JSON)
-                .when()
-                .get(fullPath);
     }
 
 
