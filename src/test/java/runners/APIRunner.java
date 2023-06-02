edc12bbe4aeb260c7b005b762d28f490e7c349cd
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-06-02 23:27:21 +0300

    02.06.23 v8 ayazicioglu

--- src/test/java/runners/APIRunner.java
+++ src/test/java/runners/APIRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = {"stepDefinitions", "hooks"},
-        tags = "@DELETE" ,
+        tags = "@Get or @Post or @Patch or @Delete" ,
         dryRun = false
 
 
